d25965cb0bee0663e080fb7de348b2074bd757cc
buihaiduongdev <haiduongtinhoc@gmail.com>
2025-04-04 15:35:08 +0700

    Fix map path

--- Spritesheets/964cd2e78344331a6a55.tsx
+++ Spritesheets/964cd2e78344331a6a55.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="964cd2e78344331a6a55" tilewidth="64" tileheight="64" tilecount="2" columns="1">
- <image source="../../../../Downloads/964cd2e78344331a6a55.jpg" width="64" height="128"/>
+ <image source="../PNG/Map/964cd2e78344331a6a55.jpg" width="64" height="128"/>
 </tileset>
